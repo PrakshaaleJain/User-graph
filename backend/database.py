0fd28d287808a978a884d125790ed977ca26275e
PrakshaaleJain <prakshaale123@gmail.com>
2025-11-02 19:21:26 +0530

    Initial commit: User & Transaction Graph System

--- backend/database.py
+++ backend/database.py
@@ -17,9 +17,9 @@ class Neo4jConnection:
             result = session.run(query, parameters or {})
             return [r.data() for r in result]
 
-# Load from environment or defaults
-NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
-NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password123")
+# # Load from environment or defaults
+# NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
+# NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
+# NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password123")
 
 db = Neo4jConnection(NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD)
