7dedf09a5368271cdec9d7acc243c416e77d3009
PrakshaaleJain <prakshaale123@gmail.com>
2025-11-02 20:43:23 +0530

    Change to database.py

--- backend/database.py
+++ backend/database.py
@@ -18,8 +18,8 @@ class Neo4jConnection:
             return [r.data() for r in result]
 
 # # Load from environment or defaults
-# NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
-# NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
-# NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password123")
+NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
+NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password123")
 
 db = Neo4jConnection(NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD)
